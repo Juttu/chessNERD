c0b5d9ec61f688ea915fba61540545d24ec4cd6c
Juttu <juttuanurag@gmail.com>
2023-07-07 00:35:51 +0530

    readitreadit

--- background.js
+++ background.js
@@ -1,77 +1,78 @@
 chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: () => {
-      var focusButton = document.getElementById("board-controls-focus");
-      if (focusButton) {
-        focusButton.click();
-      } else {
-        console.error("Focus button not found");
-      }
-
-      var boardDiv = document.getElementsByClassName("board")[0];
-      if (boardDiv) {
-        if (boardDiv.style.height === window.screen.height * 0.8 + "px") {
-          boardDiv.style.height = "";
-          boardDiv.style.width = "";
+  if (tab.url.startsWith("https://www.chess.com/game")) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: () => {
+        var focusButton = document.getElementById("board-controls-focus");
+        if (focusButton) {
+          focusButton.click();
         } else {
-          boardDiv.style.height = window.screen.height * 0.8 + "px";
-          boardDiv.style.width = window.screen.height * 0.8 + "px";
+          console.error("Focus button not found");
         }
-      } else {
-        console.error("Board div not found");
-      }
 
-      var mainDiv = document.getElementById("board-layout-main");
-
-      if (mainDiv) {
-        if (mainDiv.style.left === "40%") {
-          mainDiv.style.position = "";
-          mainDiv.style.left = "";
-          mainDiv.style.transform = "";
+        var boardDiv = document.getElementsByClassName("board")[0];
+        if (boardDiv) {
+          if (boardDiv.style.height === window.screen.height * 0.8 + "px") {
+            boardDiv.style.height = "";
+            boardDiv.style.width = "";
+          } else {
+            boardDiv.style.height = window.screen.height * 0.8 + "px";
+            boardDiv.style.width = window.screen.height * 0.8 + "px";
+          }
         } else {
-          mainDiv.style.position = "absolute";
-          mainDiv.style.left = "40%";
-          mainDiv.style.transform = "translateX(-40%)";
+          console.error("Board div not found");
         }
-      } else {
-        console.error("Main div not found");
-      }
 
-      var controlsDiv = document.getElementById("board-layout-controls");
-      if (controlsDiv) {
-        if (controlsDiv.style.display === "none") {
-          controlsDiv.style.display = "";
-        } else {
-          controlsDiv.style.display = "none";
-        }
-      } else {
-        console.error("Controls div not found");
-      }
+        var mainDiv = document.getElementById("board-layout-main");
 
-      var sidebarDivWidthParam = document.getElementsByClassName("board-layout-sidebar")[0];
-      var screenWidth = window.innerWidth;
-      if (sidebarDivWidthParam) {
-        if (sidebarDivWidthParam.style.width === screenWidth * 0.18 + "px") {
-          sidebarDivWidthParam.style.width = "";
+        if (mainDiv) {
+          if (mainDiv.style.left === "40%") {
+            mainDiv.style.position = "";
+            mainDiv.style.left = "";
+            mainDiv.style.transform = "";
+          } else {
+            mainDiv.style.position = "absolute";
+            mainDiv.style.left = "40%";
+            mainDiv.style.transform = "translateX(-40%)";
+          }
         } else {
-          sidebarDivWidthParam.style.width = screenWidth * 0.18 + "px";
+          console.error("Main div not found");
         }
-      } else {
-        console.error("SidebarParam div not found");
-      }
-      var sidebarDiv = document.getElementById('board-layout-sidebar');
-      
 
-      if (sidebarDiv) {
-        if (sidebarDiv.style.marginLeft === screenWidth / 2 + "px") {
-          sidebarDiv.style.marginLeft = "";
+        var controlsDiv = document.getElementById("board-layout-controls");
+        if (controlsDiv) {
+          if (controlsDiv.style.display === "none") {
+            controlsDiv.style.display = "";
+          } else {
+            controlsDiv.style.display = "none";
+          }
         } else {
-          sidebarDiv.style.marginLeft = screenWidth / 2 + "px";
+          console.error("Controls div not found");
         }
-      } else {
-        console.error("Sidebar div not found");
-      }
-    },
-  });
+
+        var sidebarDivWidthParam = document.getElementsByClassName("board-layout-sidebar")[0];
+        var screenWidth = window.innerWidth;
+        if (sidebarDivWidthParam) {
+          if (sidebarDivWidthParam.style.width !== screenWidth * 0.18 + "px") {
+            sidebarDivWidthParam.style.width = screenWidth * 0.18 + "px";
+          } else {
+            sidebarDivWidthParam.style.width = "";
+          }
+        } else {
+          console.error("SidebarParam div not found");
+        }
+        var sidebarDiv = document.getElementById("board-layout-sidebar");
+
+        if (sidebarDiv) {
+          if (sidebarDiv.style.marginLeft === screenWidth / 2 + "px") {
+            sidebarDiv.style.marginLeft = "";
+          } else {
+            sidebarDiv.style.marginLeft = screenWidth / 2 + "px";
+          }
+        } else {
+          console.error("Sidebar div not found");
+        }
+      },
+    });
+  }
 });
