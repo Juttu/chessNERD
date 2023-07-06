f30caeb6b023f0cce46d29b25f250843749c7cd9
Juttu <juttuanurag@gmail.com>
2023-07-06 23:11:31 +0530

    readitreadit

--- background.js
+++ background.js
@@ -6,9 +6,9 @@ chrome.action.onClicked.addListener((tab) => {
       if (focusButton) {
         focusButton.click();
       } else {
-        console.error('Focus button not found');
+        console.error("Focus button not found");
       }
-      
+
       var boardDiv = document.getElementsByClassName("board")[0];
       if (boardDiv) {
         if (boardDiv.style.height === window.screen.height * 0.8 + "px") {
@@ -19,62 +19,59 @@ chrome.action.onClicked.addListener((tab) => {
           boardDiv.style.width = window.screen.height * 0.8 + "px";
         }
       } else {
-        console.error('Board div not found');
+        console.error("Board div not found");
       }
 
-      var mainDiv = document.getElementById('board-layout-main');
-      var leftDiv = document.getElementsByClassName('board-layout-nav')[0];
-      var rightDiv = document.getElementById('board-layout-sidebar');
-      var userTaglineElements = document.getElementsByClassName('user-tagline-rating user-tagline-white');
+      var mainDiv = document.getElementById("board-layout-main");
 
       if (mainDiv) {
-        if (mainDiv.style.left === '40%') {
-          mainDiv.style.position = '';
-          mainDiv.style.left = '';
-          mainDiv.style.transform = '';
+        if (mainDiv.style.left === "40%") {
+          mainDiv.style.position = "";
+          mainDiv.style.left = "";
+          mainDiv.style.transform = "";
         } else {
-          mainDiv.style.position = 'absolute';
-          mainDiv.style.left = '40%';
-          mainDiv.style.transform = 'translateX(-40%)';
+          mainDiv.style.position = "absolute";
+          mainDiv.style.left = "40%";
+          mainDiv.style.transform = "translateX(-40%)";
         }
       } else {
-        console.error('Main div not found');
-      }
-
-      if (leftDiv && leftDiv.style.display === 'none') {
-        leftDiv.style.display = '';
-      } else if (leftDiv) {
-        leftDiv.style.display = 'none';
-      } else {
-        console.error('Left div not found');
-      }
-
-      if (rightDiv && rightDiv.style.display === 'none') {
-        rightDiv.style.display = '';
-      } else if (rightDiv) {
-        rightDiv.style.display = 'none';
-      } else {
-        console.error('Right div not found');
-      }
-
-      for (var i = 0; i < userTaglineElements.length; i++) {
-        if (userTaglineElements[i].style.display === 'none') {
-          userTaglineElements[i].style.display = '';
-        } else {
-          userTaglineElements[i].style.display = 'none';
-        }
+        console.error("Main div not found");
       }
 
       var controlsDiv = document.getElementById("board-layout-controls");
       if (controlsDiv) {
-        if (controlsDiv.style.display === 'none') {
-          controlsDiv.style.display = '';
+        if (controlsDiv.style.display === "none") {
+          controlsDiv.style.display = "";
         } else {
-          controlsDiv.style.display = 'none';
+          controlsDiv.style.display = "none";
         }
       } else {
-        console.error('Controls div not found');
+        console.error("Controls div not found");
       }
-    }
+
+      var sidebarDivWidthParam = document.getElementsByClassName("board-layout-sidebar")[0];
+      var screenWidth = window.innerWidth;
+      if (sidebarDivWidthParam) {
+        if (sidebarDivWidthParam.style.width === screenWidth * 0.18 + "px") {
+          sidebarDivWidthParam.style.width = "";
+        } else {
+          sidebarDivWidthParam.style.width = screenWidth * 0.18 + "px";
+        }
+      } else {
+        console.error("SidebarParam div not found");
+      }
+      var sidebarDiv = document.getElementById('board-layout-sidebar');
+      
+
+      if (sidebarDiv) {
+        if (sidebarDiv.style.marginLeft === screenWidth / 2 + "px") {
+          sidebarDiv.style.marginLeft = "";
+        } else {
+          sidebarDiv.style.marginLeft = screenWidth / 2 + "px";
+        }
+      } else {
+        console.error("Sidebar div not found");
+      }
+    },
   });
 });
